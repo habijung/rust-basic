7dcbcc3b3683c1be78b71cbc25a41a5a469f242d
habi <habijung0@gmail.com>
2023-02-17 12:21:15 +0900

    feat: 변수 정의 연습

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
+
+    let mut a = 100;
+    let b = 3.14;
+    let c: u32 = 12345;
+    let d: f32 = 3.14;
+    let e: bool = true;
+
+    a = a + 1;
+    println!("{} and {}", a, d);
+
+    const PI: f64 = 3.141592;
+
+    let area = PI * 5.0 * 5.0;
+    println!("{}", area);
+
+    let a = "hello";
+    println!("a: {}", a);
 }
