25df38fd67f312e9019f8dda9cece65eaf0a178a
habi <habijung0@gmail.com>
2023-02-24 12:45:54 +0900

    feat: 함수 연습
    - dbg: Debug 구현으로 소스 위치와 값을 한번에 표현함

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,11 @@
 fn main() {
     println!("Hello, world!");
 
-    let s = String::from("Hello");
-    // let s: String = "hello".to_owned();
+    let c = add(1, 2);
+    println!("{}", c);
+}
 
-    println!("{}", s);
-
-    let mut s2 = String::new(); // empty string
-    s2.push('H');
-    s2.push('i');
-    s2.push_str(" ASDF"); // 문자열 추가
-
-    println!("{}", s2);
-
-    s2 = s2.replace("Hi", "Hello");
-
-    println!("{}", s2);
-
-    let s3 = String::from("Hello World");
-
-    if s3.contains(" ") {
-        for w in s3.split_whitespace() {
-            println!("{}", w);
-        }
-    }
+fn add(a: i32, b: i32) -> i32 {
+    dbg!(a, b);
+    return a + b;
 }
