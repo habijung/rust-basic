7013c6b85f52f487c1d5361b6a7565b2d31ab50e
habi <habijung0@gmail.com>
2023-02-24 11:47:42 +0900

    feat: 문자열 &str, String 타입 연습

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 fn main() {
     println!("Hello, world!");
 
-    let arr: [i32; 3] = [1; 3];
-    println!("{}", arr[0]);
-    println!("{:?}", arr);
+    let s = String::from("Hello");
+    // let s: String = "hello".to_owned();
 
-    let dat: (i32, char, bool) = (1, 'A', true);
-    let a = dat.0;
-    let b: char = dat.1;
-    let c: bool = dat.2;
+    println!("{}", s);
 
-    let (a, b, c) = dat;
+    let mut s2 = String::new(); // empty string
+    s2.push('H');
+    s2.push('i');
+    s2.push_str(" ASDF"); // 문자열 추가
+
+    println!("{}", s2);
+
+    s2 = s2.replace("Hi", "Hello");
+
+    println!("{}", s2);
+
+    let s3 = String::from("Hello World");
+
+    if s3.contains(" ") {
+        for w in s3.split_whitespace() {
+            println!("{}", w);
+        }
+    }
 }
