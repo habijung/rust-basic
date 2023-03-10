036b75f90ae2e39b05206b88600262a93a2f6f70
habi <habijung0@gmail.com>
2023-03-10 12:10:13 +0900

    feat: impl에서 method 구현 연습

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,25 @@ impl Person {
     fn new(id: i32, name: String) -> Person {
         Person{ id: id, name: name, active: true }
     }
+
+    fn display(&self) {
+        if self.active {
+            println!("{}: {}", self.id, self.name);
+        }
+        else {
+            println!("{}: inactive", self.id);
+        }
+    }
+
+    fn set_active(&mut self, is_active: bool) {
+        self.active = is_active;
+    }
 }
 
 fn main() {
     println!("Hello, world!");
 
-    let p = Person::new(101, String::from("Tom"));
-
-    println!("{}: {}", p.id, p.name);
+    let mut p = Person::new(101, String::from("Tom"));
+    p.set_active(false);
+    p.display();
 }
