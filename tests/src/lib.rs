d5e20d246625d00c72116eeae4587ec095f1e781
habi <habijung0@gmail.com>
2023-03-17 13:16:47 +0900

    feat: Library module 생성 연습

---
+++ tests/src/lib.rs
@@ -0,0 +1,16 @@
+mod cala {
+    pub fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = super::calc::add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
