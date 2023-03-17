f88ebc905b5d31c0ec735a51236d64bcd7e73f0a
habi <habijung0@gmail.com>
2023-03-17 11:31:19 +0900

    refactor: Module을 파일로 관리하는 연습

---
+++ src/circle.rs
@@ -0,0 +1,9 @@
+const PI: f64 = 3.14;
+
+pub fn perimeter(radius: f64) -> f64 {
+    2.0 * PI * radius
+}
+
+pub fn area(radius: f64) -> f64 {
+    PI * radius * radius
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,4 @@
-mod circle {
-    const PI: f64 = 3.14;
-
-    pub fn perimeter(radius: f64) -> f64 {
-        2.0 * PI * radius
-    }
-
-    pub fn area(radius: f64) -> f64 {
-        PI * radius * radius
-    }
-}
+mod circle;
 
 fn main() {
     println!("Hello, world!");
