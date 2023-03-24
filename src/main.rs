193777ea2f3f2f260e884b9a8652b20ff9bda17c
habi <habijung0@gmail.com>
2023-03-24 11:44:07 +0900

    feat: use 기본적인 사용 연습

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
 mod shape;
 
+pub mod usa {
+    pub mod washington {
+        pub mod seattle {
+            pub fn trip() {
+                println!("Trip to Seattle");
+            }
+        }
+    }
+}
+
+use usa::washington::seattle;
+use usa::washington::seattle as s;
+use usa::washington::seattle::trip;
+
 fn main() {
     println!("Hello, world!");
 
-    shape::rect::draw();
+    // shape::rect::draw();
+
+    // Full path
+    usa::washington::seattle::trip();
+
+    // Simple path
+    seattle::trip();
+
+    // Alias path
+    s::trip();
+
+    // Use directly
+    trip();
 }
