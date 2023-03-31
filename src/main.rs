acf804d0ba32fa571c4a6564612d8e458aa04923
habi <habijung0@gmail.com>
2023-03-31 11:43:55 +0900

    feat: Trait 기본 연습

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
-struct Point<T> {
-    x: T,
-    y: T,
+trait Draw {
+    fn draw(&self, x: i32, y: i32);
 }
 
-impl<T> Point<T> {
-    fn get_x(&self) -> &T {
-        &self.x
+struct Rectangle {
+    width: i32,
+    height: i32,
+}
+
+impl Draw for Rectangle {
+    fn draw(&self, x: i32, y: i32) {
+        let x2 = x + self.width;
+        let y2 = y + self.height;
+        println!("Rect({}, {} ~ {}, {})", x, y, x2, y2);
     }
-    fn get_y(&self) -> &T {
-        &self.y
+}
+
+struct Circle {
+    radius: i32,
+}
+
+impl Draw for Circle {
+    fn draw(&self, x: i32, y: i32) {
+        println!("Circle({}, {}, {})", x, y, self.radius);
     }
+}
+
+fn draw_it(item: impl Draw, x: i32, y: i32) {
+    item.draw(x, y);
 }
 
 fn main() {
     println!("Hello, world!");
 
-    let pt = Point { x: 2.0, y: 2.0 };
+    let rect = Rectangle { width: 20, height: 20 };
+    let circle = Circle { radius: 5 };
 
-    println!("{}", pt.get_x());
-    println!("{}", pt.get_y());
+    draw_it(rect, 1, 1);
+    draw_it(circle, 2, 2);
 }
